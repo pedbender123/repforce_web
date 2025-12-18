708901647926dcf714719a6a260fae8f6ffc1b64
Pedro Bender Randon <pedro.p.bender.randon@gmail.com>
2025-12-18 18:17:05 +0000

    .

--- frontend-web/src/pages/crm/ProductList.js
+++ frontend-web/src/pages/crm/ProductList.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import apiClient from '../../../api/apiClient';
+import apiClient from '../../api/apiClient';
 import { Link, useNavigate } from 'react-router-dom';
 import { PencilIcon, TrashIcon, PlusIcon } from '@heroicons/react/24/outline';
 import { AuthContext } from '../../../context/AuthContext';
