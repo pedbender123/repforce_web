6c8ac83abb4e95f6453e0d95ac15dc5674f35617
Pedro Bender Randon <pedro.p.bender.randon@gmail.com>
2025-12-29 17:36:57 +0000

    fix: Update api import path in Webhooks.js

--- frontend-web/src/pages/system/Webhooks.js
+++ frontend-web/src/pages/system/Webhooks.js
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
-import api from '../../services/api';
+import api from '../../api/apiClient';
 import SkeletonList from '../../components/common/SkeletonList';
 
 export default function Webhooks() {
