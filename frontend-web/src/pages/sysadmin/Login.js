8002d4d9ccaa78faf2d7c3482ca9a4f48242bab4
Pedro Bender Randon <pedro.p.bender.randon@gmail.com>
2025-12-30 09:18:28 +0000

    fix: Resolve 403 by handling is_superuser in bypass logic and fixing UUID user_id parsing in Middleware

--- frontend-web/src/pages/sysadmin/Login.js
+++ frontend-web/src/pages/sysadmin/Login.js
@@ -72,7 +72,7 @@ export default function Login() {
                                 value={username}
                                 onChange={e => setUsername(e.target.value)}
                                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white px-3 py-2"
-                                placeholder="Ex: sysadmin"
+                                placeholder="Ex: admin"
                             />
                         </div>
 
