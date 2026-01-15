3dd291eb70921afbda75cf694b96bea171415979
Pedro Bender Randon <pedro.p.bender.randon@gmail.com>
2026-01-15 16:32:30 +0000

    .

--- mock-compasso/src/api.js
+++ mock-compasso/src/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
-// Use same hostname as the browser, but different port for API
-const API_HOST = window.location.hostname;
 const api = axios.create({
-    baseURL: `http://${API_HOST}:5000/api`,
+    baseURL: '/api',
 });
 
 export default api;
+
 
